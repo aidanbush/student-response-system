474286e892af184b2149e27a7a17c985746ad915
aidan <aidandbush@gmail.com>
2018-03-03 13:11:59 -0700

    Add login btn listeners with requests Also added error message functions

--- static/script.ts
+++ static/script.ts
@@ -1,8 +1,28 @@
+/* login page */
+type createRequest = {
+    class: Class;
+    person: person;
+};
+
+type joinRequest = {
+    person: person;
+};
+
+type person = {
+    name: string;
+};
+
+type Class = {
+    class_name: string;
+    class_id: string;
+};
+
+/**************************
+ * login display listeners
+ *************************/
 function onLoginJoinClick() {
     let joinDiv: HTMLElement = <HTMLElement>document.querySelector("#join");
-    if (joinDiv === null) {
-        return
-    }
+
     // check if something is already there
     if (joinDiv.innerHTML !== "") {
         joinDiv.innerHTML = "";
@@ -10,9 +30,7 @@ function onLoginJoinClick() {
     }
     // obtain the template
     let template = document.querySelector("#new_join_template");
-    if (template === null) {
-        return;
-    }
+
     // compile the template
     let func = doT.template(template.innerHTML);
     // render the data into the template
@@ -22,47 +40,12 @@ function onLoginJoinClick() {
 
     // create listener
     let joinBtn = document.querySelector("#join_class_btn");
-    if (joinBtn !== null) {
-        joinBtn.addEventListener("click", onJoinClassBtnClick);
-        console.log("add join btn listener");
-    }
-}
-
-function onJoinClassBtnClick() {
-    console.log("onJoinClassBtnClick");
-    //get info if exists
-    let classIDInput: HTMLInputElement = <HTMLInputElement>document.querySelector("#join_class_id");
-    if (classIDInput === null || classIDInput.value === "") {
-        return
-    }
-    let classID = classIDInput.value;
-
-    //get name
-    let nameInput: HTMLInputElement = <HTMLInputElement>document.querySelector("#student_name");
-    if (nameInput === null || nameInput.value === "") {
-        return
-    }
-    let name = nameInput.value;
-
-    //make request
-    let req = new XMLHttpRequest();
-
-    // response function
-    req.addEventListener("load", function () {
-        let res = JSON.parse(req.responseText);
-        console.log(res)
-    });
-
-    req.open("POST", `/api/v0/classes/${encodeURI(classID)}`);
-    req.send(name);
-    console.log(classID, name);
+    joinBtn.addEventListener("click", onJoinClassBtnClick);
+    console.log("add join btn listener");
 }
 
 function onLoginCreateClick() {
     let createDiv: HTMLElement = <HTMLElement>document.querySelector("#create");
-    if (createDiv === null) {
-        return;
-    }
 
     // check if something is already there
     if (createDiv.innerHTML !== "") {
@@ -71,26 +54,146 @@ function onLoginCreateClick() {
     }
     // obtain the template
     let template = document.querySelector("#new_create_template");
-    if (template === null) {
-        return;
-    }
+
     // compile the template
     let func = doT.template(template.innerHTML);
     // render the data into the template
     let rendered = func();
     // insert the rendered template into the DOM
     createDiv.innerHTML = rendered;
+
+    // create listener
+    let createBtn = document.querySelector("#new_class_btn");
+    createBtn.addEventListener("click", onCreateClassBtnClick);
+    console.log("add create btn listener");
 }
 
+/**********************
+ * login btn listeners
+ *********************/
+function onJoinClassBtnClick() {
+    console.log("onJoinClassBtnClick");
+    //get info if exists
+    let classIDInput: HTMLInputElement = <HTMLInputElement>document.querySelector("#join_class_id");
+    if (classIDInput.value === "") {
+        joinClassReqFail("Error: Requires class ID");
+        return;
+    }
+    let classID = classIDInput.value;
+
+    //get name
+    let nameInput: HTMLInputElement = <HTMLInputElement>document.querySelector("#student_name");
+    if (nameInput.value === "") {
+        joinClassReqFail("Error: Requires your name");
+        return;
+    }
+
+    // create request json object
+    let reqJSON: joinRequest = {
+        person: {
+            name: nameInput.value,
+        },
+    };
+
+    //make request
+    let req: XMLHttpRequest = new XMLHttpRequest();
+
+    // response listener
+    req.addEventListener("load", function () {
+        if (req.readyState === 4 && req.status === 200) {
+            let res: XMLHttpRequestResponseType = JSON.parse(req.responseText);
+            console.log("join class req success", res);
+            // TODO: implement switching pages
+            return
+        }
+        joinClassReqFail("Failed to join class");
+    });
+
+    req.open("POST", `/api/v0/classes/${encodeURI(classID)}`);
+    req.send(JSON.stringify(reqJSON));
+    console.log(reqJSON);
+}
+
+function onCreateClassBtnClick() {
+    console.log("onCreateClassBtnClick")
+
+    let classNameInput: HTMLInputElement = <HTMLInputElement>document.querySelector("#new_class_name");
+    if (classNameInput.value === "") {
+        createClassReqFail("Error: Requires class name");
+        return;
+    }
+
+    // TODO: check if have name already
+    let nameInput: HTMLInputElement = <HTMLInputElement>document.querySelector("#instructor_name");
+    if (nameInput.value === "") {
+        createClassReqFail("Error: Requires your name");
+        return;
+    }
+
+    // create request json object
+    let reqJSON: createRequest = {
+        person: {
+            name: nameInput.value,
+        },
+        class: {
+            class_name: classNameInput.value,
+            class_id: null,
+        },
+    };
+
+    // make request
+    let req: XMLHttpRequest = new XMLHttpRequest();
+
+    // response listener
+    req.addEventListener("load", function () {
+        if (req.readyState === 4 && req.status === 200) {
+            let res: XMLHttpRequestResponseType = JSON.parse(req.responseText);
+            console.log("create class req success", res);
+            // TODO: implement switching pages
+            return;
+        }
+        createClassReqFail("Error: Failed to create class");
+    });
+
+    req.addEventListener("error", function () {
+        createClassReqFail("Error: Can't connect to server");
+    });
+
+    req.addEventListener("abort", function () {
+        createClassReqFail("Error: Can't connect to server")
+    });
+
+    req.open("POST", `/api/v0/classes`);
+    req.send(JSON.stringify(reqJSON));
+    console.log(reqJSON);
+}
+
+/********************************
+ * login failed request handlers
+ *******************************/
+function joinClassReqFail(error: string) {
+    let joinDiv: HTMLElement = <HTMLElement>document.querySelector("#join_input_error");
+    joinDiv.innerHTML = error;
+
+    console.log("join class error: ", error);
+}
+
+function createClassReqFail(error: string) {
+    let classDiv: HTMLElement = <HTMLElement>document.querySelector("#new_input_error");
+    classDiv.innerHTML = error;
+
+    console.log("create class error: ", error);
+}
+
+/************
+ * listeners
+ ***********/
 function setupLoginListeners() {
-    let join_heading = document.querySelector("#join_heading");
-    if (join_heading != null){
-        join_heading.addEventListener("click", onLoginJoinClick);
-    }
+    let joinHeading = document.querySelector("#join_heading");
+    joinHeading.addEventListener("click", onLoginJoinClick);
+
     let createHeading = document.querySelector("#create_heading");
-    if (createHeading != null) {
-        createHeading.addEventListener("click", onLoginCreateClick);
-    }
+    createHeading.addEventListener("click", onLoginCreateClick);
 }
 
 function setupListeners() {
